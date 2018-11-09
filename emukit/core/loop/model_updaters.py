385d9cd9217c79a012643cfca48ed276646fa16e
Mark Pullin <marpulli@amazon.com>
2018-11-09 17:37:25 +0000

    Allow model updater to update from any field in loop state

--- emukit/core/loop/model_updaters.py
+++ emukit/core/loop/model_updaters.py
@@ -3,6 +3,7 @@
 
 
 import abc
+from typing import Callable
 
 from . import LoopState
 from ..interfaces import IModel
@@ -10,6 +11,7 @@ from ..interfaces import IModel
 
 import logging
 _log = logging.getLogger(__name__)
+
 
 class ModelUpdater(abc.ABC):
     @abc.abstractmethod
@@ -24,19 +26,27 @@ class ModelUpdater(abc.ABC):
 
 class FixedIntervalUpdater(ModelUpdater):
     """ Updates hyper-parameters every nth iteration, where n is defined by the user """
-    def __init__(self, model: IModel, interval: int = 1) -> None:
+    def __init__(self, model: IModel, interval: int=1, targets_extractor_fcn: Callable=None) -> None:
         """
         :param model: Emukit emulator model
         :param interval: Number of function evaluations between optimizing model hyper-parameters
+        :param targets_extractor_fcn: A lambda function that takes in loop state and returns the training targets.
+                                      Defaults to a function returning loop_state.Y
         """
         self.model = model
         self.interval = interval
+
+        if targets_extractor_fcn is None:
+            self.targets_extractor_fcn = lambda loop_state: loop_state.Y
+        else:
+            self.targets_extractor_fcn = targets_extractor_fcn
 
     def update(self, loop_state: LoopState) -> None:
         """
         :param loop_state: Object that contains current state of the loop
         """
-        self.model.update_data(loop_state.X, loop_state.Y)
+        targets = self.targets_extractor_fcn(loop_state)
+        self.model.update_data(loop_state.X, targets)
         if (loop_state.iteration % self.interval) == 0:
             _log.info("Updating parameters of the model")
             self.model.optimize()
