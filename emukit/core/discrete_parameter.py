b3bef0b595cdb748cd9e10595a1540f3d53deff5
Andrei Paleyes <paleyes@amazon.com>
2018-11-09 13:36:47 +0000

    Notebook and a utility API for dealing with categorical variables

--- emukit/core/discrete_parameter.py
+++ emukit/core/discrete_parameter.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-from typing import Iterable, Union
+from typing import Iterable, Union, Tuple
 
 import numpy as np
 
@@ -37,3 +37,32 @@ class InformationSourceParameter(DiscreteParameter):
         :param n_sources: Number of information sources in the problem
         """
         super().__init__('source', list(range(n_sources)))
+
+
+def create_discrete_parameter_for_categories(name: str, categories: Iterable, encodings: Iterable = None) -> Tuple:
+    """
+    Creates a DiscreteParameter object for categorical parameter.
+    If encodings corresponding to each category value are provided, they are used.
+    If encodings are not provided, one-hot encodings are generated.
+
+    :param name: Name of parameter.
+    :param categories: List of categories.
+    :param encodings: Optional list of corresponding encodings.
+    :return: A tuple (DiscreteParameter object, map of categories to encodings, map or encodings to categories)
+    """
+
+    if encodings:
+        # Encodings are given, perform some validation
+        if len(categories) != len(encodings):
+            raise ValueError("Number of categories and encodings should match. Actual sizes: {} categories and {} encodings".format(len(categories), len(encodings)))
+        if len(set(encodings)) != len(encodings):
+            raise ValueError("All encodings should be unique. Found {} duplicates.".format(len(encodings) - len(set(encodings))))
+    else:
+        # Encodings are not give, generate one-hot values
+        encodings = [10 ** i for i, _ in enumerate(categories)]
+
+    param = DiscreteParameter(name, encodings)
+    forward_dict = dict(zip(categories, encodings))
+    reverse_dict = dict(zip(encodings, categories))
+
+    return param, forward_dict, reverse_dict
