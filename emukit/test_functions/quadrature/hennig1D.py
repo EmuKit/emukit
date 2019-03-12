d6f13ef15bde47057c3547f7763d44fe633eb328
Maren Mahsereci <mahserec@amazon.com>
2019-03-12 15:53:42 +0000

    adding docstrings

--- emukit/test_functions/quadrature/hennig1D.py
+++ emukit/test_functions/quadrature/hennig1D.py
@@ -12,11 +12,11 @@ def hennig1D() -> Tuple[UserFunctionWrapper, List[Tuple[float, float]]]:
     """
     1D toy integrand coined by Philipp Hennig
 
-    Evaluates the function at x.
     .. math::
 
         e^{-x^2 -\sin^2(3x)}
 
+    :return: the wrapped test function, and the integrals bounds (defaults to interval [-3, 3]).
     """
     integral_bounds = [(-3., 3.)]
     return UserFunctionWrapper(_hennig1D), integral_bounds
@@ -24,7 +24,7 @@ def hennig1D() -> Tuple[UserFunctionWrapper, List[Tuple[float, float]]]:
 
 def _hennig1D(x: np.ndarray) -> np.ndarray:
     """
-    :param x: (num_points, 1)
+    :param x: locations for evaluation (num_points, 1)
     :return: the function values at x, shape (num_points, 1)
     """
     return np.exp(- x**2 - np.sin(3. * x)**2)
