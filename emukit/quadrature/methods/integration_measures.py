876e8d476a9953ce7f534f3a4ebc9ef3e0716f97
Maren Mahsereci <mahserec@amazon.com>
2019-03-15 11:18:32 +0000

    covariance handling in Gaussian intgeration measures

--- emukit/quadrature/methods/integration_measures.py
+++ emukit/quadrature/methods/integration_measures.py
@@ -3,7 +3,7 @@
 
 
 import numpy as np
-from typing import Tuple, List
+from typing import Tuple, List, Union
 
 
 class IntegrationMeasure:
@@ -16,16 +16,44 @@ class IntegrationMeasure:
 class GaussianMeasure(IntegrationMeasure):
     """The Gaussian measure"""
 
-    def __init__(self, mean: np.ndarray, covariance: np.ndarray):
+    def __init__(self, mean: np.ndarray, covariance: Union[float, np.ndarray]):
         """
 
         :param mean: the mean of the Gaussian
         :param covariance: the covariance matrix of the Gaussian
         """
         super().__init__('GaussianMeasure')
-        self.mean = mean
-        # Todo: check if we accept full covariance or just diagonal ones
+        self._check_input(covariance)
         self.covariance = covariance
+        self.mean = mean
+
+    def _check_input(self, covariance: Union[float, np.ndarray]) -> None:
+
+        self.isotropic = False
+        self.full_cov = False
+        self.diagonal_cov = False
+
+        # isotropic covariance
+        if isinstance(covariance, float):
+            self.isotropic = True
+        if not covariance > 0:
+            raise ValueError('Variance must be positive. Values is ' + str(covariance) + '.')
+
+        if not covariance.ndim == 2:
+            raise ValueError('Dimension of covariance must be 2d. Dimension given is ' + str(covariance.ndim) + '.')
+
+        N1, N2 = covariance.shape
+
+        # full covariance
+        if N1 == N2:
+            self.full_cov = True
+        # diagonal covariance
+        elif N2 == 1:
+            self.diagonal_cov = True
+        # none of the above
+        else:
+            raise ValueError('Shape of covariance is (' + str(N1) + ',' + str(N2) + '). Expected is 2d arrays of shape '
+                             '(N, N) or (N, 1), or a float (full, diagonal, isotropic covariance respectively).')
 
 
 class UniformMeasure(IntegrationMeasure):
