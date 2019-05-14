6c156da3da83898873bde78ffc9091a371b5524a
Aaron Klein <kleinaa@informatik.uni-freiburg.de>
2019-05-14 14:12:17 +0200

    allow for multiple event handler

--- emukit/core/loop/outer_loop.py
+++ emukit/core/loop/outer_loop.py
@@ -51,8 +51,8 @@ class OuterLoop(object):
         if self.loop_state is None:
             self.loop_state = LoopState([])
 
-        self.loop_start_event = EventHandler()
-        self.iteration_end_event = EventHandler()
+        self.loop_start_event = []
+        self.iteration_end_event = []
 
     def run_loop(self, user_function: Union[UserFunction, Callable], stopping_condition: Union[StoppingCondition, int],
                  context: dict=None) -> None:
@@ -76,7 +76,7 @@ class OuterLoop(object):
 
         _log.info("Starting outer loop")
 
-        self.loop_start_event(self, self.loop_state)
+        [e(self, self.loop_state) for e in self.loop_start_event]
 
         while not stopping_condition.should_stop(self.loop_state):
             _log.info("Iteration {}".format(self.loop_state.iteration))
@@ -85,7 +85,7 @@ class OuterLoop(object):
             new_x = self.candidate_point_calculator.compute_next_points(self.loop_state, context)
             results = user_function.evaluate(new_x)
             self.loop_state.update(results)
-            self.iteration_end_event(self, self.loop_state)
+            [e(self, self.loop_state) for e in self.iteration_end_event]
 
         self._update_models()
         _log.info("Finished outer loop")
