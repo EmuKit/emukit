91940b15e31d46e71461ff88416db9c9216ff3dd
Mark Pullin <marpulli@amazon.com>
2019-05-20 15:25:36 +0100

    Add tests to validate notebooks

---
+++ integration_tests/emukit/notebooks/test_notebooks.py
@@ -0,0 +1,33 @@
+# These are notebooks that take too long to run so are not tested here
+excluded_notebooks = ['Emukit-tutorial-multi-fidelity-bayesian-optimization.ipynb']
+
+import pytest
+import os
+
+from nbconvert.preprocessors import ExecutePreprocessor
+import nbformat
+
+notebook_directory = './notebooks/'
+
+
+def get_notebook_names():
+    """
+    Get names of all notebooks in notebook directory
+    :return:
+    """
+    extension = '.ipynb'
+
+    notebook_names = []
+    for file in os.listdir(notebook_directory):
+        if file.endswith(extension) and (file not in excluded_notebooks):
+            notebook_names.append(file)
+
+    return notebook_names
+
+
+@pytest.mark.parametrize("name", get_notebook_names())
+def test_notebook_runs_without_errors(name):
+    with open(os.path.join(notebook_directory, name)) as f:
+        nb = nbformat.read(f, as_version=4)
+    ep = ExecutePreprocessor(timeout=120)
+    ep.preprocess(nb, {'metadata': {'path': notebook_directory}})
