14408726389347c55528812dfffd3863e61d5091
Mark Pullin <33632490+marpulli@users.noreply.github.com>
2019-05-20 16:36:07 +0100

    Update emukit/core/optimization/__init__.py
    Co-Authored-By: Andrei Paleyes <apaleyes@users.noreply.github.com>

--- emukit/core/optimization/__init__.py
+++ emukit/core/optimization/__init__.py
@@ -3,7 +3,7 @@
 
 
 from .acquisition_optimizer import AcquisitionOptimizerBase  # noqa: F401
-from .context_manager import ContextManager
+from .context_manager import ContextManager  # noqa: F401
 from .gradient_acquisition_optimizer import GradientAcquisitionOptimizer  # noqa: F401
 from .local_search_acquisition_optimizer import LocalSearchAcquisitionOptimizer  # noqa: F401
 from .multi_source_acquisition_optimizer import MultiSourceAcquisitionOptimizer  # noqa: F401
