14de63acaa3e023476c3465b973ee4838e14f807
Aaron Klein <kleinaa@informatik.uni-freiburg.de>
2019-05-21 16:59:55 +0200

    add context manager

--- emukit/examples/gp_bayesian_optimization/random_search.py
+++ emukit/examples/gp_bayesian_optimization/random_search.py
@@ -1,17 +1,16 @@
 # Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-
 import numpy as np
 
-from emukit.core.loop.outer_loop import OuterLoop
 from emukit.bayesian_optimization.loops.bayesian_optimization_loop import BayesianOptimizationResults
+from emukit.core.loop.outer_loop import OuterLoop
 from emukit.core.loop.candidate_point_calculators import CandidatePointCalculator
-from emukit.experimental_design.model_free.random_design import RandomDesign
 from emukit.core.loop.loop_state import LoopState
+from emukit.core.loop.loop_state import create_loop_state
 from emukit.core.parameter_space import ParameterSpace
 from emukit.core.loop.model_updaters import ModelUpdater
-from emukit.core.loop.loop_state import create_loop_state
+from emukit.core.optimization.context_manager import ContextManager
 
 
 class RandomSampling(CandidatePointCalculator):
@@ -22,16 +21,22 @@ class RandomSampling(CandidatePointCalculator):
 
         :param parameter_space: Input space
         """
-        self.rd = RandomDesign(parameter_space=parameter_space)
+        self.parameter_space = parameter_space
 
-    def compute_next_points(self, loop_state: LoopState, context: dict=None) -> np.ndarray:
+    def compute_next_points(self, loop_state: LoopState, context_manager: ContextManager) -> np.ndarray:
         """
         :param loop_state: Object that contains current state of the loop
-        :param context: Contains variables to fix through optimization of acquisition function. The dictionary key is
-                        the parameter name and the value is the value to fix the parameter to.
-        :return: (n_points x n_dims) array of next inputs to evaluate the function at
+        :param context_manager: Optimization context manager.
+
+        :return: (1 x n_dims) array of next inputs to evaluate the function at
         """
-        return self.rd.get_samples(1)
+
+        if context_manager is not None:
+            sample = context_manager.context_free_space.sample_uniform(1)
+            sample = context_manager.expand_vector(sample)
+        else:
+            sample = self.parameter_space.sample_uniform(1)
+        return sample
 
 
 class DummyModelUpdate(ModelUpdater):
@@ -45,8 +50,8 @@ class DummyModelUpdate(ModelUpdater):
 
 
 class RandomSearch(OuterLoop):
-    def __init__(self, space: ParameterSpace, x_init: np.ndarray=None,
-                 y_init: np.ndarray=None, cost_init: np.ndarray=None):
+    def __init__(self, space: ParameterSpace, x_init: np.ndarray = None,
+                 y_init: np.ndarray = None, cost_init: np.ndarray = None):
 
         """
         Simple loop to perform random search where in each iteration points are sampled uniformly at random
@@ -71,4 +76,3 @@ class RandomSearch(OuterLoop):
 
     def get_results(self):
         return BayesianOptimizationResults(self.loop_state)
-
