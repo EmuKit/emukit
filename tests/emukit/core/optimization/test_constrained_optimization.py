353f4cb4892a71c2e0f21bfd4ea5f088054ec422
Mark Pullin <marpulli@amazon.com>
2019-05-21 18:21:41 +0100

    Uncomment tests

--- tests/emukit/core/optimization/test_constrained_optimization.py
+++ tests/emukit/core/optimization/test_constrained_optimization.py
@@ -1,51 +1,50 @@
-#import numpy as np
-#import pytest
-#
-#from emukit.core.optimization.constrained_gradient_acquisition_optimizer import (LinearInequalityConstraint,
-#                                                                                 NonlinearInequalityConstraint)
-#
-#
-#@pytest.fixture
-#def linear_constraint():
-#    A = np.array([[1., 0.5]])
-#    b_lower = np.array([-2])
-#    b_upper = np.array([3])
-#    return LinearInequalityConstraint(A, b_lower, b_upper)
-#
-#
-#@pytest.fixture
-#def nonlinear_constraint_no_jac():
-#    fun = lambda x: x[0]**2 + x[1]**2
-#    b_lower = np.array([-1])
-#    b_upper = np.array([1])
-#    return NonlinearInequalityConstraint(fun, b_lower, b_upper)
-#
-#
-#def test_linear_inequality_constraint_satisfied(linear_constraint):
-#    x_test = np.array([[1], [2]])
-#    assert linear_constraint.evaluate(x_test)[0] == 1
-#
-#
-#def test_linear_inequality_constraint_violated(linear_constraint):
-#    x_test = np.array([[1], [2]])
-#    assert linear_constraint.evaluate(x_test)[0] == 0
-#
-#
-#def test_get_scipy_linear_constraint(linear_constraint):
-#    scipy_constraint = linear_constraint.get_scipy_constraint()
-#    assert np.array_equal(scipy_constraint.A, np.array([[1., 0.5]]))
-#    assert np.array_equal(scipy_constraint.lb, np.array([-2]))
-#    assert np.array_equal(scipy_constraint.ub, np.array([3]))
-#
-#
-#def test_nonlinear_inequality_constraint_satisfied(nonlinear_constraint_no_jac):
-#    assert nonlinear_constraint_no_jac.evaluate(np.array([[0, 0]])) == 1
-#
-#
-#def test_nonlinear_inequality_constraint_violated(nonlinear_constraint_no_jac):
-#    assert nonlinear_constraint_no_jac.evaluate(np.array([[1, 1]])) == 0
-#
-#
-#def test_get_scipy_nonlinear_constraint(nonlinear_constraint_no_jac):
-#    nonlinear_constraint_no_jac.get_scipy_constraint()
-#
+import numpy as np
+import pytest
+
+from emukit.core.optimization.constrained_gradient_acquisition_optimizer import (LinearInequalityConstraint,
+                                                                                 NonlinearInequalityConstraint)
+
+
+@pytest.fixture
+def linear_constraint():
+    A = np.array([[1., 0.5]])
+    b_lower = np.array([-2])
+    b_upper = np.array([3])
+    return LinearInequalityConstraint(A, b_lower, b_upper)
+
+
+@pytest.fixture
+def nonlinear_constraint_no_jac():
+    fun = lambda x: x[0]**2 + x[1]**2
+    b_lower = np.array([-1])
+    b_upper = np.array([1])
+    return NonlinearInequalityConstraint(fun, b_lower, b_upper)
+
+
+def test_linear_inequality_constraint_satisfied(linear_constraint):
+    x_test = np.array([[1], [2]])
+    assert linear_constraint.evaluate(x_test)[0] == 1
+
+
+def test_linear_inequality_constraint_violated(linear_constraint):
+    x_test = np.array([[1], [2]])
+    assert linear_constraint.evaluate(x_test)[0] == 0
+
+
+def test_get_scipy_linear_constraint(linear_constraint):
+    scipy_constraint = linear_constraint.get_scipy_constraint()
+    assert np.array_equal(scipy_constraint.A, np.array([[1., 0.5]]))
+    assert np.array_equal(scipy_constraint.lb, np.array([-2]))
+    assert np.array_equal(scipy_constraint.ub, np.array([3]))
+
+
+def test_nonlinear_inequality_constraint_satisfied(nonlinear_constraint_no_jac):
+    assert nonlinear_constraint_no_jac.evaluate(np.array([[0, 0]])) == 1
+
+
+def test_nonlinear_inequality_constraint_violated(nonlinear_constraint_no_jac):
+    assert nonlinear_constraint_no_jac.evaluate(np.array([[1, 1]])) == 0
+
+
+def test_get_scipy_nonlinear_constraint(nonlinear_constraint_no_jac):
+    nonlinear_constraint_no_jac.get_scipy_constraint()
