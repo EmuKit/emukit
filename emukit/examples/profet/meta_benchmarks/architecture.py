8a2fc9dcb6711d32e7328158a0d6cf0ac7e38254
Aaron Klein <kleinaa@informatik.uni-freiburg.de>
2019-05-22 19:30:21 +0200

    add arch for regression

--- emukit/examples/profet/meta_benchmarks/architecture.py
+++ emukit/examples/profet/meta_benchmarks/architecture.py
@@ -22,6 +22,25 @@ def get_default_architecture_classification(input_dimensionality: int) -> torch.
     return Architecture(n_inputs=input_dimensionality)
 
 
+def get_default_architecture_regression(input_dimensionality: int) -> torch.nn.Module:
+    class Architecture(nn.Module):
+        def __init__(self, n_inputs, n_hidden=500):
+            super(Architecture, self).__init__()
+            self.fc1 = nn.Linear(n_inputs, n_hidden)
+            self.fc2 = nn.Linear(n_hidden, n_hidden)
+            self.fc3 = nn.Linear(n_hidden, 2)
+            self.sigma_layer = AppendLayer(noise=1e-3)
+
+        def forward(self, x):
+            x = torch.tanh(self.fc1(x))
+            x = torch.tanh(self.fc2(x))
+            x = self.fc3(x)
+            mean = x[:, None, 0]
+            return self.sigma_layer(mean)
+
+    return Architecture(n_inputs=input_dimensionality)
+
+
 def get_default_architecture_cost(input_dimensionality: int) -> torch.nn.Module:
     class Architecture(nn.Module):
         def __init__(self, n_inputs, n_hidden=500):
