91f540f784d91a99af5efa339e169f1cefb165dc
polivucci <polivucci1@sheffield.ac.uk>
2019-06-03 13:07:07 +0100

    Update probability_of_improvement.py

--- emukit/bayesian_optimization/acquisitions/probability_of_improvement.py
+++ emukit/bayesian_optimization/acquisitions/probability_of_improvement.py
@@ -67,3 +67,56 @@ class ProbabilityOfImprovement(Acquisition):
     @property
     def has_gradients(self):
         return isinstance(self.model, IDifferentiable)
+    
+    
+    
+class ProbabilityOfFeasibility(Acquisition):
+
+    def __init__(self, model: Union[IModel, IDifferentiable], jitter: np.float64 = np.float64(0)) -> None:
+        """
+        This acquisition computes for a given input point the probability of satisfying the constraint
+        C<0. For more information see:
+
+        Michael A. Gelbart, Jasper Snoek, and Ryan P. Adams,
+        Bayesian Optimization with Unknown Constraints,
+        https://arxiv.org/pdf/1403.5607.pdf
+
+        :param model: The underlying model that provides the predictive mean and variance for the given test points
+        :param jitter: Jitter to balance exploration / exploitation
+        """
+        self.model = model
+        self.jitter = jitter
+
+    def evaluate(self, x: np.ndarray) -> np.ndarray:
+        """
+        Computes the probability of improving over the current best
+
+        :param x: points where the acquisition is evaluated.
+        """
+        mean, variance = self.model.predict(x)
+        standard_deviation = np.sqrt(variance)
+        _, cdf, _ = get_quantiles(self.jitter, 0, mean, standard_deviation)
+
+        return cdf
+
+    def evaluate_with_gradients(self, x: np.ndarray) -> Tuple:
+        """
+        Computes the  probability of improving over the current best and its derivative
+
+        :param x: points where the acquisition is evaluated.
+        """
+        mean, variance = self.model.predict(x)
+        standard_deviation = np.sqrt(variance)
+
+        dmean_dx, dvariance_dx = self.model.get_prediction_gradients(x)
+        dstandard_devidation_dx = dvariance_dx / (2 * standard_deviation)
+
+        pdf, cdf, u = get_quantiles(self.jitter, 0, mean, standard_deviation)
+
+        dcdf_dx = - (pdf / standard_deviation) * (dmean_dx + dstandard_devidation_dx * u)
+
+        return cdf, dcdf_dx
+
+    @property
+    def has_gradients(self):
+        return isinstance(self.model, IDifferentiable)
