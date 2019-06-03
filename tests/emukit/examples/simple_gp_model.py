7fa88067970cd6f7093b013828e82e0869b59d89
Mark Pullin <33632490+marpulli@users.noreply.github.com>
2019-06-03 10:53:30 +0100

    Update tests/emukit/examples/simple_gp_model.py
    Co-Authored-By: Andrei Paleyes <apaleyes@users.noreply.github.com>

--- tests/emukit/examples/simple_gp_model.py
+++ tests/emukit/examples/simple_gp_model.py
@@ -12,7 +12,7 @@ def test_simple_gp_model_optimize():
 
 def test_simple_gp_model_predict():
     x = np.linspace(-1, 1, 3)[:, None]
-    y = x **2
+    y = x**2
     gp = SimpleGaussianProcessModel(x, y)
     mean, var = gp.predict(np.array(5, 1))
     assert mean.shape == (5, 1)
