f8555536953fe33c7e856447dc08a31a2dfefd3e
polivucci <polivucci1@sheffield.ac.uk>
2019-06-03 16:01:13 +0100

    Update probability_of_improvement.py

--- emukit/bayesian_optimization/acquisitions/probability_of_improvement.py
+++ emukit/bayesian_optimization/acquisitions/probability_of_improvement.py
@@ -17,11 +17,9 @@ class ProbabilityOfImprovement(Acquisition):
         """
         This acquisition computes for a given input point the probability of improving over the
         currently best observed function value. For more information see:
-
         Efficient Global Optimization of Expensive Black-Box Functions
         Jones, Donald R. and Schonlau, Matthias and Welch, William J.
         Journal of Global Optimization
-
         :param model: The underlying model that provides the predictive mean and variance for the given test points
         :param jitter: Jitter to balance exploration / exploitation
         """
@@ -31,7 +29,6 @@ class ProbabilityOfImprovement(Acquisition):
     def evaluate(self, x: np.ndarray) -> np.ndarray:
         """
         Computes the probability of improving over the current best
-
         :param x: points where the acquisition is evaluated.
         """
         mean, variance = self.model.predict(x)
@@ -45,7 +42,6 @@ class ProbabilityOfImprovement(Acquisition):
     def evaluate_with_gradients(self, x: np.ndarray) -> Tuple:
         """
         Computes the  probability of improving over the current best and its derivative
-
         :param x: points where the acquisition is evaluated.
         """
         mean, variance = self.model.predict(x)
@@ -67,10 +63,9 @@ class ProbabilityOfImprovement(Acquisition):
     @property
     def has_gradients(self):
         return isinstance(self.model, IDifferentiable)
+
     
-    
-    
-class ProbabilityOfFeasibility(Acquisition):
+ class ProbabilityOfFeasibility(Acquisition):
 
     def __init__(self, model: Union[IModel, IDifferentiable], jitter: np.float64 = np.float64(0)) -> None:
         """
@@ -90,19 +85,18 @@ class ProbabilityOfFeasibility(Acquisition):
     def evaluate(self, x: np.ndarray) -> np.ndarray:
         """
         Computes the probability of improving over the current best
-
         :param x: points where the acquisition is evaluated.
         """
         mean, variance = self.model.predict(x)
-        standard_deviation = np.sqrt(variance)
-        _, cdf, _ = get_quantiles(self.jitter, 0, mean, standard_deviation)
+        mean += self.jitter
 
+        standard_deviation = np.sqrt(variance)
+        cdf = scipy.stats.norm.cdf(0, mean, standard_deviation)
         return cdf
 
     def evaluate_with_gradients(self, x: np.ndarray) -> Tuple:
         """
         Computes the  probability of improving over the current best and its derivative
-
         :param x: points where the acquisition is evaluated.
         """
         mean, variance = self.model.predict(x)
@@ -111,9 +105,11 @@ class ProbabilityOfFeasibility(Acquisition):
         dmean_dx, dvariance_dx = self.model.get_prediction_gradients(x)
         dstandard_devidation_dx = dvariance_dx / (2 * standard_deviation)
 
-        pdf, cdf, u = get_quantiles(self.jitter, 0, mean, standard_deviation)
-
-        dcdf_dx = - (pdf / standard_deviation) * (dmean_dx + dstandard_devidation_dx * u)
+        mean += self.jitter
+        u = - mean / standard_deviation
+        pdf = scipy.stats.norm.pdf(0, mean, standard_deviation)
+        cdf = scipy.stats.norm.cdf(0, mean, standard_deviation)
+        dcdf_dx = - pdf * (dmean_dx + dstandard_devidation_dx * u)
 
         return cdf, dcdf_dx
 
