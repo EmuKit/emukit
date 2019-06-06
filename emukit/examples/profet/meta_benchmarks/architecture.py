fix normalizing targets

emukit/examples/profet/meta_benchmarks/architecture.py
@@ -9,14 +9,14 @@ def get_default_architecture_classification(input_dimensionality: int) -> torch.
             super(Architecture, self).__init__()
             self.fc1 = nn.Linear(n_inputs, n_hidden)
             self.fc2 = nn.Linear(n_hidden, n_hidden)
-            self.fc3 = nn.Linear(n_hidden, 2)
+            self.fc3 = nn.Linear(n_hidden, 1)
             self.sigma_layer = AppendLayer(noise=1e-3)
 
         def forward(self, x):
             x = torch.tanh(self.fc1(x))
             x = torch.tanh(self.fc2(x))
             x = self.fc3(x)
-            mean = (torch.tanh(x[:, None, 0]) + 1) / 2
+            mean = torch.sigmoid(x)
             return self.sigma_layer(mean)
 
     return Architecture(n_inputs=input_dimensionality)
@@ -28,14 +28,14 @@ def get_default_architecture_regression(input_dimensionality: int) -> torch.nn.M
             super(Architecture, self).__init__()
             self.fc1 = nn.Linear(n_inputs, n_hidden)
             self.fc2 = nn.Linear(n_hidden, n_hidden)
-            self.fc3 = nn.Linear(n_hidden, 2)
+            self.fc3 = nn.Linear(n_hidden, 1)
             self.sigma_layer = AppendLayer(noise=1e-3)
 
         def forward(self, x):
             x = torch.tanh(self.fc1(x))
             x = torch.tanh(self.fc2(x))
             x = self.fc3(x)
-            mean = x[:, None, 0]
+            mean = x
             return self.sigma_layer(mean)
 
     return Architecture(n_inputs=input_dimensionality)
@@ -47,14 +47,14 @@ def get_default_architecture_cost(input_dimensionality: int) -> torch.nn.Module:
             super(Architecture, self).__init__()
             self.fc1 = nn.Linear(n_inputs, n_hidden)
             self.fc2 = nn.Linear(n_hidden, n_hidden)
-            self.fc3 = nn.Linear(n_hidden, 2)
+            self.fc3 = nn.Linear(n_hidden, 1)
             self.sigma_layer = AppendLayer(noise=1e-3)
 
         def forward(self, x):
             x = torch.tanh(self.fc1(x))
             x = torch.tanh(self.fc2(x))
             x = self.fc3(x)
-            mean = x[:, None, 0]
+            mean = x
             return self.sigma_layer(mean)
 
     return Architecture(n_inputs=input_dimensionality)

emukit/examples/profet/meta_benchmarks/meta_svm.py
@@ -30,8 +30,8 @@ def meta_svm(fname_objective: str, fname_cost: str, noise: bool = True) -> Tuple
 
     x_mean_objective = data["x_mean"]
     x_std_objective = data["x_std"]
-    y_mean_objective = data["y_mean"]
-    y_std_objective = data["y_std"]
+    # y_mean_objective = data["y_mean"]
+    # y_std_objective = data["y_std"]
     task_feature_objective = data["task_feature"]
     objective = get_default_architecture_classification(x_mean_objective.shape[0]).float()
     objective.load_state_dict(data["state_dict"])
@@ -52,8 +52,10 @@ def meta_svm(fname_objective: str, fname_cost: str, noise: bool = True) -> Tuple
         x = np.concatenate((config, Ht), axis=1)
         x_norm = torch.from_numpy((x - x_mean_objective) / x_std_objective).float()
         o = objective.forward(x_norm).data.numpy()
-        m = o[:, 0] * y_std_objective + y_mean_objective
-        log_v = o[:, 1] * y_std_objective ** 2
+        # m = o[:, 0] * y_std_objective + y_mean_objective
+        # log_v = o[:, 1] * y_std_objective ** 2
+        m = o[:, 0]
+        log_v = o[:, 1]
         if with_noise:
             feval = np.random.randn() * np.sqrt(np.exp(log_v)) + m
         else: