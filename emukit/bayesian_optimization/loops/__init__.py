74ba9e236352ff9e116b5c2970399fb2af6ed4b2
polivucci <polivucci1@sheffield.ac.uk>
2019-06-26 12:07:42 +0100

    Update __init__.py

--- emukit/bayesian_optimization/loops/__init__.py
+++ emukit/bayesian_optimization/loops/__init__.py
@@ -4,3 +4,4 @@
 
 from .bayesian_optimization_loop import BayesianOptimizationLoop  # noqa: F401
 from .unknown_constraint_bayesian_optimization_loop import UnknownConstraintBayesianOptimizationLoop  # noqa: F401
+from .constrained_loop_state import UnknownConstraintLoopState  # noqa: F401
