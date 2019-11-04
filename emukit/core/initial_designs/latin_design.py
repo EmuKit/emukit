1e7e1986c326141427e0df7b66db44cdaf0bf0da
Andrei Paleyes <paleyes@amazon.com>
2019-11-04 18:00:04 +0000

    Added docstrings and types

--- emukit/core/initial_designs/latin_design.py
+++ emukit/core/initial_designs/latin_design.py
@@ -9,16 +9,28 @@ except ImportError:
     raise ImportError('pyDOE needs to be installed in order to use latin design')
 
 from .base import ModelFreeDesignBase
+from .. import ParameterSpace
 
 
 class LatinDesign(ModelFreeDesignBase):
     """
     Latin hypercube experiment design.
+
+    Based on pyDOE implementation. For further reference see https://pythonhosted.org/pyDOE/randomized.html#latin-hypercube
     """
-    def __init__(self, parameter_space):
+    def __init__(self, parameter_space: ParameterSpace) -> None:
+        """
+        :param parameter_space: The parameter space to generate design for.
+        """
         super(LatinDesign, self).__init__(parameter_space)
 
-    def get_samples(self, point_count):
+    def get_samples(self, point_count: int) -> np.ndarray:
+        """
+        Generates requested amount of points.
+
+        :param point_count: Number of points required.
+        :return: A numpy array with shape (point_count x space_dim)
+        """
         bounds = self.parameter_space.get_bounds()
         X_design_aux = pyDOE.lhs(len(bounds), point_count, criterion='center')
         ones = np.ones((X_design_aux.shape[0], 1))
