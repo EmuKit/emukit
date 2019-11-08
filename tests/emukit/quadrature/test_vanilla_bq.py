dfaa3f639e98773ebae475bef11fa2c08094ddaa
Maren Mahsereci <mahserec@amazon.com>
2019-11-08 18:24:36 +0000

    removing unnecessary stuff

--- tests/emukit/quadrature/test_vanilla_bq.py
+++ tests/emukit/quadrature/test_vanilla_bq.py
@@ -10,10 +10,7 @@ import pytest
 
 from emukit.quadrature.interfaces.base_gp import IBaseGaussianProcess
 from emukit.quadrature.methods.vanilla_bq import VanillaBayesianQuadrature
-from emukit.quadrature.kernels.bounds import BoxBounds
-from emukit.quadrature.kernels.quadrature_kernels import QuadratureKernel
 from emukit.quadrature.kernels import QuadratureRBFLebesgueMeasure
-from emukit.core.continuous_parameter import ContinuousParameter
 from emukit.model_wrappers.gpy_quadrature_wrappers import RBFGPy, BaseGaussianProcessGPy
 
 
@@ -85,28 +82,6 @@ def test_vanilla_bq_transformations():
     assert_array_equal(method.transform(Y), Y)
     assert_array_equal(method.inverse_transform(method.transform(Y)), Y)
     assert_array_equal(method.transform(method.inverse_transform(Y)), Y)
-
-
-def test_vanilla_bq_model():
-    X_train = np.random.rand(5, 2)
-    Y_train = np.random.rand(5, 1)
-
-    mock_cparam = mock.create_autospec(ContinuousParameter)
-    mock_bounds = mock.create_autospec(BoxBounds)
-    mock_bounds.convert_to_list_of_continuous_parameters.return_value = 2 * [mock_cparam]
-    mock_kern = mock.create_autospec(QuadratureKernel, integral_bounds=mock_bounds)
-    mock_gp = mock.create_autospec(IBaseGaussianProcess, kern=mock_kern, X=X_train, Y=Y_train)
-    method = VanillaBayesianQuadrature(base_gp=mock_gp, X=X_train, Y=Y_train)
-
-    assert_array_equal(method.X, X_train)
-    assert_array_equal(method.Y, Y_train)
-    assert_array_equal(method.integral_bounds.bounds, mock_bounds.bounds)
-    # we assert that the integral bounds are the same in the method and the quadrature kernel, since all integration
-    # happens in the kernel. The test is restrictive but easier to do for the init than behavioral test. There are some
-    # behavioral tests for setting new bounds in the vanilla_bq method and the bounds itself. Setting the integral
-    # bounds should be done with care.
-    assert method.integral_bounds == mock_bounds
-    assert method.integral_parameters == 2 * [mock_cparam]
 
 
 def test_vanilla_bq_integrate(vanilla_bq):
