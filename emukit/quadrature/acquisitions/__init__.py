657f4a06c21be4dd957991b4ad619e2fa19d63f2
Maren Mahsereci <mahserec@amazon.com>
2019-11-08 18:42:02 +0000

    adding import

--- emukit/quadrature/acquisitions/__init__.py
+++ emukit/quadrature/acquisitions/__init__.py
@@ -6,4 +6,3 @@ from .squared_correlation import SquaredCorrelation  # noqa: F401
 from .squared_correlation import SquaredCorrelation as IntegralVarianceReduction  # noqa: F401
 from .mutual_information import MutualInformation  # noqa: F401
 from .uncertainty_sampling import UncertaintySampling  # noqa: F401
-
