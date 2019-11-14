374ceefd4f40263185fde77c5b395cc01d089d37
Maren Mahsereci <42842079+mmahsereci@users.noreply.github.com>
2019-11-14 11:34:00 +0100

    uncertainty sampling acquisition for bq (#264)

--- tests/emukit/quadrature/test_measures.py
+++ tests/emukit/quadrature/test_measures.py
@@ -4,8 +4,13 @@
 
 import numpy as np
 import pytest
+from math import isclose
 
 from emukit.quadrature.kernels.integration_measures import UniformMeasure, IsotropicGaussianMeasure
+
+
+REL_TOL = 1e-5
+ABS_TOL = 1e-4
 
 
 def test_uniform_measure_shapes():
@@ -30,6 +35,16 @@ def test_uniform_measure_wrong_bounds():
 
     with pytest.raises(ValueError):
         UniformMeasure(bounds)
+
+
+def test_uniform_measure_gradients():
+    measure_bounds = [(-1, 2), (0, 1)]
+    measure = UniformMeasure(bounds=measure_bounds)
+
+    N = 3
+    D = len(measure_bounds)
+    x = np.reshape(np.random.randn(D * N), [N, D])
+    _check_grad(measure, x)
 
 
 def test_iso_gauss_measure_shapes():
@@ -60,3 +75,33 @@ def test_iso_gauss_measure_invalid_input():
 
     with pytest.raises(ValueError):
         IsotropicGaussianMeasure(mean=mean_wrong_dim, variance=1.)
+
+
+def test_iso_gauss_measure_gradients():
+    D = 2
+    measure = IsotropicGaussianMeasure(mean=np.random.randn(D), variance=np.random.randn()**2)
+
+    N = 3
+    x = np.reshape(np.random.randn(D * N), [N, D])
+    _check_grad(measure, x)
+
+
+def _compute_numerical_gradient(m, x, eps=1e-6):
+    f = m.compute_density(x)
+    grad = m.compute_density_gradient(x)
+
+    grad_num = np.zeros(grad.shape)
+    for d in range(x.shape[1]):
+        x_tmp = x.copy()
+        x_tmp[:, d] = x_tmp[:, d] + eps
+        f_tmp = m.compute_density(x_tmp)
+        grad_num_d = (f_tmp - f) / eps
+        grad_num[:, d] = grad_num_d
+    return grad, grad_num
+
+
+def _check_grad(aq, x):
+    grad, grad_num = _compute_numerical_gradient(aq, x)
+    isclose_all = 1 - np.array([isclose(grad[i, j], grad_num[i, j], rel_tol=REL_TOL, abs_tol=ABS_TOL)
+                                for i in range(grad.shape[0]) for j in range(grad.shape[1])])
+    assert isclose_all.sum() == 0
