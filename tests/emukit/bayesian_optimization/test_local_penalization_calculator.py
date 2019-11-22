6c0909d17a81d71b60eed0f3b60262c025cc99e7
Andrei Paleyes <paleyes@amazon.com>
2019-11-22 14:28:11 +0000

    Added validation to LP calculator
    plus fixed typo
    plus added test for batch bayes opt

--- tests/emukit/bayesian_optimization/test_local_penalization_calculator.py
+++ tests/emukit/bayesian_optimization/test_local_penalization_calculator.py
@@ -1,10 +1,14 @@
 import GPy
 import numpy as np
-from emukit.core.loop.loop_state import create_loop_state
+
+import mock
+import pytest
 
 from emukit.bayesian_optimization.acquisitions import ExpectedImprovement
 from emukit.bayesian_optimization.local_penalization_calculator import LocalPenalizationPointCalculator
 from emukit.core import ParameterSpace, ContinuousParameter
+from emukit.core.interfaces import IModel
+from emukit.core.loop.loop_state import create_loop_state
 from emukit.core.optimization import GradientAcquisitionOptimizer
 from emukit.model_wrappers import GPyModelWrapper
 
@@ -24,3 +28,16 @@ def test_local_penalization():
     loop_state = create_loop_state(x_init, y_init)
     new_points = lp_calc.compute_next_points(loop_state)
     assert new_points.shape == (batch_size, 1)
+
+
+def test_local_penalization_requires_gradients():
+    parameter_space = ParameterSpace([ContinuousParameter('x', 0, 1)])
+    acquisition_optimizer = GradientAcquisitionOptimizer(parameter_space)
+
+    model = mock.create_autospec(IModel)
+
+    acquisition = ExpectedImprovement(model)
+    batch_size = 5
+
+    with pytest.raises(ValueError):
+        LocalPenalizationPointCalculator(acquisition, acquisition_optimizer, model, parameter_space, batch_size)
