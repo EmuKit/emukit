a1bf88e7897ea792a96b863e20cd4b3249aa217b
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2020-03-31 14:20:16 -0700

    added Latin and Sobol to initial design __init__

--- emukit/core/initial_designs/__init__.py
+++ emukit/core/initial_designs/__init__.py
@@ -2,3 +2,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .random_design import RandomDesign  # noqa: F401
+from .latin_design import LatinDesign  # noqa: F401
+from .sobol_design import SobolDesign  # noqa: F401
