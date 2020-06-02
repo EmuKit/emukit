51111da0314334e99a2dc4f11705d38d586a4287
claire <claire@claire.localdomain>
2020-06-02 09:21:49 +0200

    remove comments

--- tests/emukit/core/optimization/test_context_manager.py
+++ tests/emukit/core/optimization/test_context_manager.py
@@ -43,9 +43,6 @@ def catg_context():
 def test_context_manager_catg_expand_vector(catg_space, catg_context):
     context_manager = ContextManager(catg_space, catg_context)
     x = np.array([[4., -1], [3, 0.]])
-    print(context_manager.non_context_idxs)
-    print(context_manager.context_idxs)
-
     x_expanded = context_manager.expand_vector(x)
     assert context_manager.space.dimensionality == 11
     assert x_expanded.shape == (2, context_manager.space.dimensionality)
