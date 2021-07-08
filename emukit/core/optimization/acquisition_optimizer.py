e79f759a66d547342442c8dd9b3ee3fabe1e3acd
Neil Dalchau <ndalchau@microsoft.com>
2021-07-08 12:47:59 +0100

    Reintroducing warning for out-of-domain contexts in optimization.

--- emukit/core/optimization/acquisition_optimizer.py
+++ emukit/core/optimization/acquisition_optimizer.py
@@ -24,6 +24,20 @@ class AcquisitionOptimizerBase(abc.ABC):
         """
         self.space = space
 
+    def _validate_context_parameters(self, context: Dict[str, any]):
+        for context_name, context_value in context.items():
+            # Check parameter exists in space
+            if context_name not in self.space.parameter_names:
+                raise ValueError(context_name + ' appears as variable in context but not in the parameter space.')
+
+            # Log warning if context parameter is out of domain
+            param = self.space.get_parameter_by_name(context_name)
+            if not hasattr(param, 'encoding'):
+                if param.check_in_domain(context_value):
+                    _log.info(f'Parameter {context_name} fixed to {context_value}')
+                else:
+                    _log.warning(f'{context_name} with value {context_value} is out of the domain')
+
     @abc.abstractmethod
     def _optimize(self, acquisition: Acquisition, context_manager: ContextManager) -> Tuple[np.ndarray, np.ndarray]:
         """
@@ -46,6 +60,8 @@ class AcquisitionOptimizerBase(abc.ABC):
         """
         if context is None:
             context = dict()
+        else:
+            self._validate_context_parameters(context)
         context_manager = ContextManager(self.space, context)
         max_x, max_value = self._optimize(acquisition, context_manager)
 
