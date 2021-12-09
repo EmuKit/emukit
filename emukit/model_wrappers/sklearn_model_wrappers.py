dc7de4608822738d21085689d7058d22a6e19761
Andrei Paleyes <apalees@gmail.com>
2021-12-09 13:45:59 +0000

    Updated wrapper, added tests

--- emukit/model_wrappers/sklearn_model_wrappers.py
+++ emukit/model_wrappers/sklearn_model_wrappers.py
@@ -1,90 +1,55 @@
-import numpy
 from typing import Tuple
 
+import numpy as np
+try:
+    from sklearn.gaussian_process import GaussianProcessRegressor
+except ImportError:
+    ImportError('scikit-learn needs to be installed in order to use SklearnGPRWrapper')
+
 from emukit.core.interfaces.models import IModel
-from sklearn.gaussian_process import GaussianProcessRegressor
 
-class sklearnGPRWrapper(GaussianProcessRegressor, IModel):
+class SklearnGPRWrapper(IModel):
 
-    def predict(self, X: numpy.ndarray) -> Tuple[numpy.ndarray, numpy.ndarray]:
+    def __init__(self, sklearn_model: GaussianProcessRegressor):
+        """
+        :param sklearn_model: Scikit-learn GPR model to wrap
+        """
+        self.model = sklearn_model
+
+
+    def predict(self, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """
         Predict mean and variance values for given points
+
         :param X: array of shape (n_points x n_inputs) of points to run prediction for
         :return: Tuple of mean and variance which are 2d arrays of shape (n_points x n_outputs)
-        
         """
-        return super(sklearnGPRWrapper,self).predict(X, return_cov=True)
+        mean, std = self.model.predict(X, return_std=True)
+        return mean, np.power(std, 2.0).reshape(-1, 1)
 
-    
-    def set_data(self, X: numpy.ndarray, Y: numpy.ndarray) -> None:
+
+    def set_data(self, X: np.ndarray, Y: np.ndarray) -> None:
         """
         Sets training data in model
         :param X: new points
         :param Y: function values at new points X
         
         """
-        self.X_train_, self.y_train_ = X, Y
-        
-        
+        self.model.X_train_, self.model.y_train_ = X, Y
+
+
     def optimize(self) -> None:
         """
         Optimize hyper-parameters of model
-        
-        sklearn does the optimization inside the fit() method:
-        https://github.com/scikit-learn/scikit-learn/blob/95119c13af77c76e150b753485c662b7c52a41a2/sklearn/gaussian_process/_gpr.py#L222
-        
         """
-        if self.optimizer is not None and self.kernel_.n_dims > 0:
-            # Choose hyperparameters based on maximizing the log-marginal
-            # likelihood (potentially starting from several initial values)
-            def obj_func(theta, eval_gradient=True):
-                if eval_gradient:
-                    lml, grad = self.log_marginal_likelihood(
-                        theta, eval_gradient=True, clone_kernel=False)
-                    return -lml, -grad
-                else:
-                    return -self.log_marginal_likelihood(theta,
-                                                         clone_kernel=False)
+        self.model.fit(self.X, self.Y)
 
-            # First optimize starting from theta specified in kernel
-            optima = [(self._constrained_optimization(obj_func,
-                                                      self.kernel_.theta,
-                                                      self.kernel_.bounds))]
 
-            # Additional runs are performed from log-uniform chosen initial
-            # theta
-            if self.n_restarts_optimizer > 0:
-                if not numpy.isfinite(self.kernel_.bounds).all():
-                    raise ValueError(
-                        "Multiple optimizer restarts (n_restarts_optimizer>0) "
-                        "requires that all bounds are finite.")
-                bounds = self.kernel_.bounds
-                for iteration in range(self.n_restarts_optimizer):
-                    theta_initial = \
-                        self._rng.uniform(bounds[:, 0], bounds[:, 1])
-                    optima.append(
-                        self._constrained_optimization(obj_func, theta_initial,
-                                                       bounds))
-            # Select result from run with minimal (negative) log-marginal
-            # likelihood
-            lml_values = list(map(itemgetter(1), optima))
-            self.kernel_.theta = optima[numpy.argmin(lml_values)][0]
-            self.kernel_._check_bounds_params()
-
-            self.log_marginal_likelihood_value_ = -numpy.min(lml_values)
-        else:
-            self.log_marginal_likelihood_value_ = \
-                self.log_marginal_likelihood(self.kernel_.theta,
-                                             clone_kernel=False)
-
-        
-        
-        
     @property
-    def X(self):
-        return self.X_train_
+    def X(self) -> np.ndarray:
+        return self.model.X_train_
 
-    
+
     @property
-    def Y(self):
-        return self.y_train_
+    def Y(self) -> np.ndarray:
+        return self.model.y_train_
---
+++ integration_tests/emukit/models/test_sklearn_model_wrapper.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pytest
+from sklearn.gaussian_process import GaussianProcessRegressor
+
+from emukit.model_wrappers.sklearn_model_wrappers import SklearnGPRWrapper
+
+
+@pytest.fixture
+def model():
+    rng = np.random.RandomState(42)
+    x_init = rng.rand(5, 2)
+    y_init = rng.rand(5, 1)
+
+    sklearn_model = GaussianProcessRegressor()
+    model = SklearnGPRWrapper(sklearn_model)
+    model.set_data(x_init, y_init)
+    model.optimize()
+    return model
+
+
+def test_predict_shape(model):
+    rng = np.random.RandomState(43)
+    x_test = rng.rand(10, 2)
+
+    m, v = model.predict(x_test)
+    assert m.shape == (10, 1)
+    assert v.shape == (10, 1)
+
+
+def test_update_data(model):
+    rng = np.random.RandomState(43)
+    x_new = rng.rand(5, 2)
+    y_new = rng.rand(5, 1)
+    model.set_data(x_new, y_new)
+
+    assert model.X.shape == x_new.shape
+    assert model.Y.shape == y_new.shape
