c9ea3e12fbe2fbafdbf3ec6ea300edef34048671
Maren Mahsereci <maren.mhsrc@gmail.com>
2022-06-08 17:19:57 +0200

    adjusting quad example

--- emukit/examples/vanilla_bayesian_quadrature_with_rbf/vanilla_bq_loop_with_rbf.py
+++ emukit/examples/vanilla_bayesian_quadrature_with_rbf/vanilla_bq_loop_with_rbf.py
@@ -17,41 +17,49 @@ from emukit.quadrature.typing import BoundsType
 def create_vanilla_bq_loop_with_rbf_kernel(
     X: np.ndarray,
     Y: np.ndarray,
-    integral_bounds: Optional[BoundsType],
-    measure: Optional[IntegrationMeasure],
+    integral_bounds: Optional[BoundsType] = None,
+    measure: Optional[IntegrationMeasure] = None,
     rbf_lengthscale: float = 1.0,
     rbf_variance: float = 1.0,
 ) -> VanillaBayesianQuadratureLoop:
+    """Creates a quadrature loop with a standard (vanilla) model.
+
+    :param X: Initial training point locations, shape (n_points, input_dim).
+    :param Y: Initial training point function values, shape (n_points, 1).
+    :param integral_bounds: List of d tuples, where d is the dimensionality of the integral and the tuples contain the
+                            lower and upper bounds of the integral
+                            i.e., [(lb_1, ub_1), (lb_2, ub_2), ..., (lb_d, ub_d)].
+                            Only used if ``measure`` is not given in which case the unnormalized Lebesgue measure is used.
+    :param measure: An integration measure. Either ``measure`` or ``integral_bounds`` must be given.
+                    If both ``integral_bounds`` and ``measure`` are given, ``integral_bounds`` is disregarded.
+    :param rbf_lengthscale: The lengthscale of the rbf kernel, defaults to 1.
+    :param rbf_variance: The variance of the rbf kernel, defaults to 1.
+    :return: The vanilla BQ loop.
+
     """
 
-    :param X: initial training point locations, shape (n_points, input_dim)
-    :param Y:  initial training point function values, shape (n_points, 1)
-    :param integral_bounds: List of input_dim tuples, where input_dim is the dimensionality of the integral
-    and the tuples contain the lower and upper bounds of the integral i.e.,
-    [(lb_1, ub_1), (lb_2, ub_2), ..., (lb_D, ub_D)]. None means infinite bounds.
-    :param measure: the integration measure. None means the standard Lebesgue measure is used.
-    :param rbf_lengthscale: the lengthscale of the rbf kernel, defaults to 1.
-    :param rbf_variance: the variance of the rbf kernel, defaults to 1.
-    :return: The vanilla BQ loop
-    """
+    if measure is not None and measure.input_dim != X.shape[1]:
+        raise ValueError(
+            f"Dimensionality of measure ({measure.input_dim}) does not match the dimensionality of "
+            f"the data ({X.shape[1]})."
+        )
 
     if (integral_bounds is not None) and (len(integral_bounds) != X.shape[1]):
-        D_bounds = len(integral_bounds)
-        input_dim = X.shape[1]
         raise ValueError(
-            "dimension of integral bounds provided ({}) does not match the input dimension of X ({}).".format(
-                D_bounds, input_dim
-            )
+            f"Dimension of integral bounds ({len(integral_bounds)}) does not match the input dimension "
+            f"of X ({X.shape[1]})."
         )
+
     if rbf_lengthscale <= 0:
-        raise ValueError("rbf lengthscale must be positive. The current value is {}.".format(rbf_lengthscale))
+        raise ValueError(f"rbf lengthscale must be positive. The current value is {rbf_lengthscale}.")
     if rbf_variance <= 0:
-        raise ValueError("rbf variance must be positive. The current value is {}.".format(rbf_variance))
+        raise ValueError(f"rbf variance must be positive. The current value is {rbf_variance}.")
 
     gpy_model = GPy.models.GPRegression(
         X=X, Y=Y, kernel=GPy.kern.RBF(input_dim=X.shape[1], lengthscale=rbf_lengthscale, variance=rbf_variance)
     )
 
+    # This function handles the omittion if the integral bounds in case measure is also given.
     emukit_model = create_emukit_model_from_gpy_model(
         gpy_model=gpy_model, integral_bounds=integral_bounds, measure=measure
     )
