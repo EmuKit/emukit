0f659083d9c5b47474a3326a5f7ff5bf00248282
Maren Mahsereci <maren.mhsrc@gmail.com>
2022-12-14 18:02:51 +0100

    adding fixed seed to test

--- integration_tests/emukit/bayesian_optimization/test_constrained_loop.py
+++ integration_tests/emukit/bayesian_optimization/test_constrained_loop.py
@@ -8,6 +8,7 @@ from emukit.test_functions import branin_function
 
 
 def test_optimization_with_linear_constraint():
+    np.random.seed(0)
     branin_fcn, parameter_space = branin_function()
     x_init = parameter_space.sample_uniform(10)
     y_init = branin_fcn(x_init)
