25eebfa126c654d28d7c1b60fac1bcc18d5e655a
Maren Mahsereci <maren.mhsrc@gmail.com>
2024-03-27 17:15:27 +0100

    black formatting

--- emukit/examples/preferential_batch_bayesian_optimization/__init__.py
+++ emukit/examples/preferential_batch_bayesian_optimization/__init__.py
@@ -1,4 +1,2 @@
 # Copyright 2024 The Emukit Authors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-
-
--- emukit/sensitivity/__init__.py
+++ emukit/sensitivity/__init__.py
@@ -3,5 +3,3 @@
 
 # Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-
-
