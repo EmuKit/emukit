df270b6f91c6bf00c79620a946dbd3db93affaac
Andrei Paleyes <apalees@gmail.com>
2025-12-19 11:35:35 +0000

    Add integ tests install, fix a warning, fix linting

--- tests/emukit/core/test_outer_loop.py
+++ tests/emukit/core/test_outer_loop.py
@@ -5,10 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-import pytest
-
 import mock
 import numpy as np
+import pytest
 
 from emukit.core import ContinuousParameter, ParameterSpace
 from emukit.core.interfaces import IModel
