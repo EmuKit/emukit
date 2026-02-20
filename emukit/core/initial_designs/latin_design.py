bc980dee88e2fd262c82cf22b5587853bcf001b0
Alejandro Gomez <alejandro@meetiqm.com>
2026-02-20 15:23:42 +0200

    Fix project

--- emukit/core/initial_designs/latin_design.py
+++ emukit/core/initial_designs/latin_design.py
@@ -8,7 +8,7 @@
 import numpy as np
 
 try:
-    import pyDOE
+    import pydoe
 except ImportError:
     raise ImportError("pyDOE needs to be installed in order to use latin design")
 
@@ -37,7 +37,7 @@ class LatinDesign(InitialDesignBase):
         :return: A numpy array of generated samples, shape (point_count x space_dim)
         """
         bounds = self.parameter_space.get_bounds()
-        X_design_aux = pyDOE.lhs(len(bounds), point_count, criterion="center")
+        X_design_aux = pydoe.lhs(len(bounds), point_count, criterion="center")
         ones = np.ones((X_design_aux.shape[0], 1))
 
         lower_bound = np.asarray(bounds)[:, 0].reshape(1, len(bounds))
